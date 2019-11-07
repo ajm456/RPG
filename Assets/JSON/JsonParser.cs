5209a5955bfa0fab24c6e10468f153032f32a9c6
Andrew Matthews <ajmatthews845@gmail.com>
2019-11-07 00:24:03 +0000

    Shuffle around JSON structures
    Remove the JSON-based UI stuff as that needs some thinking.
    
    Add support for (and sample) JSON-defined abilities.

---
+++ Assets/JSON/JsonParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JsonParser
+{
+	private const string JSON_ABILITIES_ROOT = "Assets/JSON/abilities/";
+
+	// Load all JSON-specified abilities in the JSON_ABILITIES_ROOT folder into
+	// a dictionary where the ability name is the key.
+	public static Dictionary<string, Ability> LoadAllAbilities() {
+		Dictionary<string, Ability> abilityDict = new Dictionary<string, Ability>();
+
+		foreach(string filename in Directory.EnumerateFiles(JSON_ABILITIES_ROOT)) {
+			// Watch out for Unity-generated .meta files
+			if(filename.Contains(".meta")) {
+				continue;
+			}
+			Debug.Log("Reading from file: " + filename);
+			string json = File.ReadAllText(filename);
+			Ability ability = JsonUtility.FromJson<Ability>(json);
+			abilityDict.Add(ability.name, ability);
+		}
+
+		return abilityDict;
+	}
+}
---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Ability
+{
+	// Abilities (how does this interact with auras and such?)
+	public string name;
+	public int calmAdj, discordAdj, hpAdjMin, hpAdjMax;
+
+	public Ability(string name, int calmAdj, int discordAdj, int hpAdjMin, int hpAdjMax) {
+		this.name = name;
+		this.calmAdj = calmAdj;
+		this.discordAdj = discordAdj;
+		this.hpAdjMin = hpAdjMin;
+		this.hpAdjMax = hpAdjMax;
+	}
+
+	public override string ToString() {
+		return "[abilityName: " + name + ", hpAdj: " + hpAdjMin + " to " + hpAdjMax + "]";
+	}
+}
