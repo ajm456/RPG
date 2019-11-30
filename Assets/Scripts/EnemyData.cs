14f011bc338abab7ca50f63f04d2105cd2657322
Andrew Matthews <ajmatthews845@gmail.com>
2019-11-30 13:14:18 +0000

    Update hero/enemy JSON deserializer classes for new agility stat

--- Assets/Scripts/EnemyData.cs
+++ Assets/Scripts/EnemyData.cs
@@ -3,14 +3,15 @@
 public class EnemyDataStrAbilities
 {
 	public string name;
-	public int maxHp, strength, behaviourIndex;
+	public int maxHp, strength, agility, behaviourIndex;
 	public List<string> abilityNames;
 
-	public EnemyDataStrAbilities(string name, int maxHp, int strength, int behaviourIndex, List<string> abilityNames)
+	public EnemyDataStrAbilities(string name, int maxHp, int strength, int agility, int behaviourIndex, List<string> abilityNames)
 	{
 		this.name = name;
 		this.maxHp = maxHp;
 		this.strength = strength;
+		this.agility = agility;
 		this.behaviourIndex = behaviourIndex;
 		this.abilityNames = abilityNames;
 	}
@@ -21,21 +22,22 @@ public class EnemyData
 	private static readonly List<Ability> allAbilities = JsonParser.LoadAllAbilities();
 
 	public string name;
-	public int maxHp, strength, behaviourIndex;
+	public int maxHp, strength, agility, behaviourIndex;
 	public List<Ability> calmAbilities, discordAbilities;
 
-	public EnemyData(EnemyDataStrAbilities enemy)
+	public EnemyData(EnemyDataStrAbilities wrapper)
 	{
-		name = enemy.name;
-		maxHp = enemy.maxHp;
-		strength = enemy.strength;
-		behaviourIndex = enemy.behaviourIndex;
+		name = wrapper.name;
+		maxHp = wrapper.maxHp;
+		strength = wrapper.strength;
+		agility = wrapper.agility;
+		behaviourIndex = wrapper.behaviourIndex;
 
 		// Filter to find this character's abilities
 		calmAbilities = new List<Ability>();
 		foreach (Ability ability in allAbilities)
 		{
-			if (enemy.abilityNames.Contains(ability.name))
+			if (wrapper.abilityNames.Contains(ability.name))
 			{
 				if (ability.isCalm)
 					calmAbilities.Add(ability);
--- Assets/Scripts/HeroData.cs
+++ Assets/Scripts/HeroData.cs
@@ -5,16 +5,17 @@ public class HeroDataJsonWrapper
 {
 	public string name;
 	public List<int> color;
-	public int hp, maxHp, strength, calm, discord;
+	public int hp, maxHp, strength, agility, calm, discord;
 	public List<string> abilityNames;
 
-	public HeroDataJsonWrapper(string name, List<int> color, int hp, int maxHp, int strength, int calm, int discord, List<string> abilityNames)
+	public HeroDataJsonWrapper(string name, List<int> color, int hp, int maxHp, int strength, int agility, int calm, int discord, List<string> abilityNames)
 	{
 		this.name = name;
 		this.color = color;
 		this.hp = hp;
 		this.maxHp = maxHp;
 		this.strength = strength;
+		this.agility = agility;
 		this.calm = calm;
 		this.discord = discord;
 		this.abilityNames = abilityNames;
@@ -27,25 +28,26 @@ public class HeroData
 
 	public string name;
 	public Color color;
-	public int hp, maxHp, strength, calm, discord;
+	public int hp, maxHp, strength, agility, calm, discord;
 	public List<Ability> calmAbilities, discordAbilities;
 
-	public HeroData(HeroDataJsonWrapper hero)
+	public HeroData(HeroDataJsonWrapper wrapper)
 	{
-		name = hero.name;
-		color = new Color(hero.color[0]/255f, hero.color[1]/255f, hero.color[2]/255f);
-		hp =  hero.hp;
-		maxHp = hero.maxHp;
-		strength = hero.strength;
-		calm = hero.calm;
-		discord = hero.discord;
+		name = wrapper.name;
+		color = new Color(wrapper.color[0]/255f, wrapper.color[1]/255f, wrapper.color[2]/255f);
+		hp =  wrapper.hp;
+		maxHp = wrapper.maxHp;
+		strength = wrapper.strength;
+		agility = wrapper.agility;
+		calm = wrapper.calm;
+		discord = wrapper.discord;
 
 		// Filter to find this hero's abilities
 		calmAbilities = new List<Ability>();
 		discordAbilities = new List<Ability>();
 		foreach (Ability ability in allAbilities)
 		{
-			if (hero.abilityNames.Contains(ability.name))
+			if (wrapper.abilityNames.Contains(ability.name))
 			{
 				if (ability.isCalm)
 					calmAbilities.Add(ability);
