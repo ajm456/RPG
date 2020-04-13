ecc7db9db2ece9de456da633db402a0df30def11
Chris <squagonal@gmail.com>
2020-04-13 20:11:46 +0100

    Add basic movement

---
+++ Assets/Scripts/HeroOverworldMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroOverworldMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    //Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f){
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+            if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f){
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
+        }
+    }
+}
