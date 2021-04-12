cb74e2ffd05c47988231241705fbda3b12e99ed3
Andrew Matthews <ajmatthews845@gmail.com>
2021-04-12 18:09:22 +0100

    Some initial testing of postprocessing shaders.
    Or "image effects" as Unity calls them. Looking to implement some cool image effects to transition into battle scenes, initially thinking some kind of blur like in FFX boss battles.

---
+++ Assets/Scripts/Overworld/BattleBlur.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BattleBlur : MonoBehaviour
+{
+	[SerializeField]
+	private float intensity;
+
+	private Material material;
+	
+	void Awake()
+	{
+		// Create a private material to be used for the effect
+		material = new Material(Shader.Find("Hidden/BattleBlur"));
+	}
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		// Postprocess the image
+		if (intensity == 0)
+		{
+			Graphics.Blit(source, destination);
+			return;
+		}
+
+		material.SetFloat("_bwBlend", intensity);
+		Graphics.Blit(source, destination, material);
+	}
+}
